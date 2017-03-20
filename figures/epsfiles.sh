b8510e952baba9ab7816e3ab0ec0117e012fa74f
Neil Eliot <neil@eliot.org.uk>
2017-03-20 09:08:18 +0000

    First Updates

---
+++ figures/epsfiles.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+for f in *.png
+do
+	NAME=`echo "$f" | cut -d'.' -f1`
+	echo "Converting $f To $NAME.eps"
+	/usr/bin/convert $f $NAME.eps
+done
+
+for f in *.jpg
+do
+	NAME=`echo "$f" | cut -d'.' -f1`
+	echo "Converting $f To $NAME.eps"
+	/usr/bin/convert $f $NAME.eps
+done
